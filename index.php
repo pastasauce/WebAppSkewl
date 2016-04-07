2bad2a370c7c0911f60c394effdac689861fe442
Jeppe Stensturp <jepstrup@live.dk>
2016-04-07 08:15:40 +0200

    Update 2
    I dunno

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <title>WebApp 1</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/core.css" />
-    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
-    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-	  <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+    <link rel="stylesheet" href="jquery-mobile/jquery.mobile-1.4.5.min.css" />
+    <script src="jquery-mobile/jquery-1.11.1.min.js"></script>
+	  <script src="jquery-mobile/jquery.mobile-1.4.5.min.js"></script>
   </head>
   <body>
     <div data-role="page" id="page" data-theme="a">
